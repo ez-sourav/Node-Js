34b13a1d91355b51a80144e6da9c7db8a2222638
ez-sourav <souravb2003@gmail.com>
2025-11-13 20:50:00 +0530

    modules

---
+++ 02-Modules/module.js
@@ -0,0 +1,14 @@
+export function add(a, b){
+    return a + b;
+}
+
+export function sub(a, b){
+    return a - b;
+}
+
+// Default/Multiple export
+// module.exports = {add,sub}
+
+// export using 'exports' Object
+// exports.add = (a,b) => a+b
+// exports.sub = (a,b) => a-b
