152fbd6619dc9b71a6799ff9bc41f2c9acbb9c7d
ez-sourav <souravb2003@gmail.com>
2025-11-14 18:07:16 +0530

    commit

---
+++ 04-HTTP-Server/index.js
@@ -0,0 +1,20 @@
+const http = require('http')
+const fs = require('fs')
+const myServer = http.createServer((req,res)=>{
+    const log = `${new Date().toLocaleString()} : ${req.url} New Request Received \n`
+    fs.appendFile('./log.txt',log,(err,data)=>{
+        switch(req.url){
+            case('/'):res.end("Home Page")
+            break
+            case('/about'):res.end("I am Sourav Biswas")
+            break
+            default:res.end('404 NOt Found')
+                
+        }
+    })
+    console.log(req);
+    console.log("New request received");
+})
+myServer.listen(3000,()=>{
+    console.log('Server Started');
+})
