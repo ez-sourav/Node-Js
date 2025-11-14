41ea3dd7f0a18cebb113984884d7e4c9d5bb6886
ez-sourav <souravb2003@gmail.com>
2025-11-14 18:08:40 +0530

    small changes in index.js

--- 04-HTTP-Server/index.js
+++ 04-HTTP-Server/index.js
@@ -1,19 +1,22 @@
-const http = require('http')
-const fs = require('fs')
-const myServer = http.createServer((req,res)=>{
-    const log = `${new Date().toLocaleString()} : ${req.url} New Request Received \n`
-    fs.appendFile('./log.txt',log,(err,data)=>{
-        switch(req.url){
-            case('/'):res.end("Home Page")
-            break
-            case('/about'):res.end("I am Sourav Biswas")
-            break
-            default:res.end('404 NOt Found')
-                
-        }
-    })
-    
-})
-myServer.listen(3000,()=>{
-    console.log('Server Started');
-})
+const http = require("http");
+const fs = require("fs");
+const myServer = http.createServer((req, res) => {
+  const log = `${new Date().toLocaleString()} : ${
+    req.url
+  } New Request Received \n`;
+  fs.appendFile("./log.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("Home Page");
+        break;
+      case "/about":
+        res.end("I am Sourav Biswas");
+        break;
+      default:
+        res.end("404 NOt Found");
+    }
+  });
+});
+myServer.listen(3000, () => {
+  console.log("Server Started");
+});
