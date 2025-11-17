63b6bf5626e82672f919ae4f6d77b745f68385a2
ez-sourav <souravb2003@gmail.com>
2025-11-17 12:44:33 +0530

    Grouping routes

--- 06-RestAPI/index.js
+++ 06-RestAPI/index.js
@@ -17,11 +17,22 @@ app.get("/api/users", (req, res) => {
   return res.json(users);
 });
 
-app.get('/api/users/:id',(req,res)=>{
+
+// if route same so we can do this type 
+app.route('/api/users/:id').get((req,res)=>{
     const id =parseInt(req.params.id);
     const user = users.find(user => user.id === id)
     return res.json(user)
 })
+.patch((req,res)=>{
+    // Edit user with ID
+    return  res.json({status:"Pending"})
+})
+.delete((req,res)=>{
+    // Delete user with ID
+    return  res.json({status:"Pending"})
+})
+
 
 app.post('/api/users',(req,res)=>{
     //TODO: Create new user
@@ -29,18 +40,27 @@ app.post('/api/users',(req,res)=>{
         status:"Pending"
     });
 })
-app.patch('/api/users/:id',(req,res)=>{
-    //TODO: Edit the user with id
-    return res.json({
-        status:"Pending"
-    });
-})
-app.delete('/api/users/:id',(req,res)=>{
-    //TODO: Delete the user with id
-    return res.json({
-        status:"Pending"
-    });
-})
+
+// app.get('/api/users/:id',(req,res)=>{
+//     const id =parseInt(req.params.id);
+//     const user = users.find(user => user.id === id)
+//     return res.json(user)
+// })
+
+// app.patch('/api/users/:id',(req,res)=>{
+//     //TODO: Edit the user with id
+//     return res.json({
+//         status:"Pending"
+//     });
+// })
+// app.delete('/api/users/:id',(req,res)=>{
+//     //TODO: Delete the user with id
+//     return res.json({
+//         status:"Pending"
+//     });
+// })
+
+
 
 app.listen(3000, () => {
   console.log("Server started");
