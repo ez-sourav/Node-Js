4ca2cb1e77bdf350063e6d129de1191080bad5c5
ez-sourav <souravb2003@gmail.com>
2025-11-19 19:19:48 +0530

    create model and connections

--- 06-RestAPI/index.js
+++ 06-RestAPI/index.js
@@ -6,40 +6,53 @@ const { type } = require("os");
 
 const app = express();
 
+// Connection mongoose
+mongoose
+  .connect("mongodb://127.0.0.1:27017/sourav-1")
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => console.log("Mongo error : ", err));
+
 // Schema
 const userSchema = new mongoose.Schema({
-  firstName:{
-    type :String,
-    required:true,
+  firstName: {
+    type: String,
+    required: true,
   },
-  lastName:{
-    type :String,
+  lastName: {
+    type: String,
   },
-  email:{
-    type :String,
-    required:true,
-    unique:true,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
   },
-  jobTitle:{
-    type:String,
+  jobTitle: {
+    type: String,
   },
-  gender:{
-    type:String
-  }
-})
+  gender: {
+    type: String,
+  },
+},{timestamps:true});
 
+// Model
+const User = mongoose.model("user", userSchema);
 
 //Middleware - Plugin
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req,res,next) =>{
-  fs.appendFile('./log.txt',`\n ${new Date().toLocaleString()} : IP Address - ${req.ip}, Method:${req.method}, Path : ${req.path}\n`,(err,data)=>{
-   next() 
-    })
-  
-})
-
-
+app.use((req, res, next) => {
+  fs.appendFile(
+    "./log.txt",
+    `\n ${new Date().toLocaleString()} : IP Address - ${req.ip}, Method:${
+      req.method
+    }, Path : ${req.path}\n`,
+    (err, data) => {
+      next();
+    }
+  );
+});
 
 app.get("/users", (req, res) => {
   const html = `
@@ -52,7 +65,7 @@ app.get("/users", (req, res) => {
 
 //Routes
 app.get("/api/users", (req, res) => {
-  res.setHeader("X-MyName","Sourav Biswas");
+  res.setHeader("X-MyName", "Sourav Biswas");
   return res.json(users);
 });
 
@@ -62,8 +75,8 @@ app
   .get((req, res) => {
     const id = parseInt(req.params.id);
     const user = users.find((user) => user.id === id);
-    if(!user){
-      res.status(404).json({error:"User Not Found"})
+    if (!user) {
+      res.status(404).json({ error: "User Not Found" });
     }
     return res.json(user);
   })
@@ -110,17 +123,44 @@ app
     });
   });
 
-app.post("/api/users", (req, res) => {
+app.post("/api/users", async(req, res) => {
   //TODO: Create new user
   const body = req.body;
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  ) {
+    return res.status(400).json({ msg: "All fields are required" });
+  }
 
-  users.push({ id: users.length + 1, ...body });
-  fs.writeFile("./USER_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.status(201).json({
-      status: "Success",
-      id: users.length,
-    });
+  // users.push({ id: users.length + 1, ...body });
+  // fs.writeFile("./USER_DATA.json", JSON.stringify(users), (err, data) => {
+  //   return res.status(201).json({
+  //     status: "Success",
+  //     id: users.length,
+  //   });
+  // });
+
+  // now we do help of mongoose 
+
+  const result  = await User.create({
+    firstName:body.first_name,
+    lastName:body.last_name,
+    email:body.email,
+    gender:body.gender,
+    jobTitle:body.job_title,
   });
+
+
+  return res.status(201).json({
+      status: "Success",
+      
+    });
+
 });
 
 // app.get('/api/users/:id',(req,res)=>{
