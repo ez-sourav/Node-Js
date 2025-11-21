8a1968fed6158a053998304c272b1820e792138e
ez-sourav <souravb2003@gmail.com>
2025-11-21 19:49:03 +0530

    commit

--- 07-Short-URL/index.js
+++ 07-Short-URL/index.js
@@ -1,6 +1,34 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const { connectToMongoDb } = require("./connect");
+const urlRoute = require("./routes/url");
+const URL = require("./models/url");
 
-app.listen(3000,()=>{
-    console.log("Server Started");
-})
+connectToMongoDb("mongodb://localhost:27017/short-url").then(() =>
+  console.log("MongoDb Connected")
+);
+
+app.use(express.json());
+
+app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+            timestamp:new Date().toLocaleString(),
+        }, 
+      },
+    }
+  );
+  res.redirect(entry.redirectURL)
+});
+
+app.listen(3000, () => {
+  console.log("Server Started");
+});
