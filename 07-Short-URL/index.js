9cd2f056b8670a4c1961e2ceeeb2d1dcdd5b7841
ez-sourav <souravb2003@gmail.com>
2025-11-22 17:29:51 +0530

    SSR

--- 07-Short-URL/index.js
+++ 07-Short-URL/index.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const app = express();
+const path = require('path')
 const { connectToMongoDb } = require("./connect");
 const urlRoute = require("./routes/url");
 const URL = require("./models/url");
+const staticRoute = require('./routes/staticRouter')
 
 connectToMongoDb("mongodb://localhost:27017/short-url").then(() =>
   console.log("MongoDb Connected")
 );
 
+app.set('view engine','ejs')
+app.set('views',path.resolve('./views'))
+
 app.use(express.json());
 
+// app.get("/test", async (req, res) => {
+//   const allUrls = await URL.find({});
+//   return res.render('home',{
+//     urls:allUrls
+//   });
+// });
+
 app.use("/url", urlRoute);
+
+app.use('/',staticRoute);
 
 app.get("/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
@@ -21,12 +35,12 @@ app.get("/:shortId", async (req, res) => {
     {
       $push: {
         visitHistory: {
-            timestamp:new Date().toLocaleString(),
-        }, 
+          timestamp: new Date().toLocaleString(),
+        },
       },
     }
   );
-  res.redirect(entry.redirectURL)
+  res.redirect(entry.redirectURL);
 });
 
 app.listen(3000, () => {
