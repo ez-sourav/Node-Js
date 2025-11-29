30cd176c0323e6f975e10c0133263670843e4a5f
ez-sourav <souravb2003@gmail.com>
2025-11-29 19:13:31 +0530

    add command

--- 08-Discord-Bot-YT/command.js
+++ 08-Discord-Bot-YT/command.js
@@ -7,4 +7,4 @@ const commands = [
     description: 'Replies with Pong!',
   },
 ];
-const rest = new REST({ version: '10' }).setToken(TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
