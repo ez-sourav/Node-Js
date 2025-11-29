4424dc9df0e7cf51d597ad1131672f586659cba5
ez-sourav <souravb2003@gmail.com>
2025-11-29 19:12:52 +0530

    add command

--- 08-Discord-Bot-YT/command.js
+++ 08-Discord-Bot-YT/command.js
@@ -1,7 +1,10 @@
 import { REST, Routes } from 'discord.js';
+import dotenv from "dotenv";
+dotenv.config();
 const commands = [
   {
     name: 'ping',
     description: 'Replies with Pong!',
   },
 ];
+const rest = new REST({ version: '10' }).setToken(TOKEN);
