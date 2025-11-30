71ba1453abfe9d81df8052baa2fdb6b4c6241fc8
ez-sourav <souravb2003@gmail.com>
2025-11-30 11:32:35 +0530

    add commnd message

--- 08-Discord-Bot-YT/command.js
+++ 08-Discord-Bot-YT/command.js
@@ -8,3 +8,12 @@ const commands = [
   },
 ];
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+try {
+  console.log('Started refreshing application (/) commands.');
+
+  await rest.put(Routes.applicationCommands('1443826865486827563'), { body: commands });
+
+  console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+  console.error(error);
+}
