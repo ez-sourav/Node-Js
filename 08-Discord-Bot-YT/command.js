6736964b7160aa5c2a2208f64a0a2b0b5aa108f7
ez-sourav <souravb2003@gmail.com>
2025-11-30 11:43:11 +0530

    commit

--- 08-Discord-Bot-YT/command.js
+++ 08-Discord-Bot-YT/command.js
@@ -1,19 +1,21 @@
-import { REST, Routes } from 'discord.js';
+import { REST, Routes } from "discord.js";
 import dotenv from "dotenv";
 dotenv.config();
 const commands = [
   {
-    name: 'create',
-    description: 'Create a new short URL',
+    name: "create",
+    description: "Create a new short URL",
   },
 ];
-const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 try {
-  console.log('Started refreshing application (/) commands.');
+  console.log("Started refreshing application (/) commands.");
 
-  await rest.put(Routes.applicationCommands('1443826865486827563'), { body: commands });
+  await rest.put(Routes.applicationCommands("1443826865486827563"), {
+    body: commands,
+  });
 
-  console.log('Successfully reloaded application (/) commands.');
+  console.log("Successfully reloaded application (/) commands.");
 } catch (error) {
   console.error(error);
 }
