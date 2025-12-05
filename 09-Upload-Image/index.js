5d1e9b1b0eb2cb8d2ebe1c543da48181a0cf43f3
ez-sourav <souravb2003@gmail.com>
2025-12-05 11:36:36 +0530

    add multer and post route

--- 09-Upload-Image/index.js
+++ 09-Upload-Image/index.js
@@ -1,18 +1,24 @@
 const path = require("path");
+const multer  = require('multer')
 const express = require("express");
 
 const app = express();
 const PORT = 3000;
 
+const upload = multer({ dest: 'uploads/' })
 
 app.set("view engine","ejs");
 app.set('views',path.resolve('./views'));
 
-app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 app.get("/", (req, res) => {
   return res.render("homepage");
 });
+
+app.post('/upload',upload.single(''),(req, res) => {
+  
+})
 
 app.listen(PORT, () => {
   console.log('Server running on',PORT);
