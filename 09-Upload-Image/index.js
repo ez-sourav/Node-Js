16c7f566459d4110c91c594185aba6280c10b664
ez-sourav <souravb2003@gmail.com>
2025-12-05 11:01:56 +0530

    add path

--- 09-Upload-Image/index.js
+++ 09-Upload-Image/index.js
@@ -1,9 +1,17 @@
+const path = require("path");
 const express = require("express");
+
 const app = express();
 const PORT = 3000;
 
+
+app.set("view engine","ejs");
+app.set('views',path.resolve('./views'));
+
+app.use(express.json())
+
 app.get("/", (req, res) => {
-  res.send("Hello Express!");
+  return res.render("homepage");
 });
 
 app.listen(PORT, () => {
