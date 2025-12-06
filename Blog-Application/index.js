7ab6d267d9463cea3bf85fce2eaa44e63d6f39a6
ez-sourav <souravb2003@gmail.com>
2025-12-06 13:24:02 +0530

    commit

--- Blog-Application/index.js
+++ Blog-Application/index.js
@@ -1,8 +1,12 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT,() =>{
-    console.log("Server Started ",PORT);
-})
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.listen(PORT, () => {
+  console.log("Server Started ", PORT);
+});
