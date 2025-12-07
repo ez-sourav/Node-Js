8eeeaef3daba48b6dba0d4221de6620213542b9f
ez-sourav <souravb2003@gmail.com>
2025-12-07 16:59:53 +0530

    add static function

--- Blog-Application/models/user.js
+++ Blog-Application/models/user.js
@@ -44,7 +44,23 @@ userSchema.pre("save", function () {
 
   this.salt = salt;
   this.password = hashedPassword;
+});
 
+userSchema.static("matchPassword",async function (email, password) {
+  const user = await this.findOne({ email });
+  if (!user) throw new Error("User Not Found");
+
+  const salt = user.salt;
+  const hashedPassword = user.password;
+
+  const userProvidedHash = createHmac("sha256", salt)
+    .update(password)
+    .digest("hex");
+
+  if (hashedPassword !== userProvidedHash)
+    throw new Error("Incorrect Password");
+
+  return user;
 });
 
 const User = model("user", userSchema);
