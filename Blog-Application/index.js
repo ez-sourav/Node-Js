c4ee210b32730609b7a0fdb6535fcd4ef403942c
ez-sourav <souravb2003@gmail.com>
2025-12-10 20:34:48 +0530

    add blog route

--- Blog-Application/index.js
+++ Blog-Application/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cookieParser = require('cookie-parser')
 
 const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 const app = express();
@@ -27,6 +28,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
 app.listen(PORT, () => {
   console.log("Server Started ", PORT);
 });
