c08e1b2b68e5285233183e95d6978ce868f9ebf0
ez-sourav <souravb2003@gmail.com>
2025-12-10 20:12:07 +0530

    add blog schema

---
+++ Blog-Application/models/blog.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+const blogSchema = new Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    body:{
+        type:String,
+        required:true,
+    },
+    coverImage:{
+        type:String,
+        required:false,
+    },
+    createdBy:{
+        type:Schema.Types.ObjectId,
+        ref: "user",
+    }
+},{timestamps:true});
