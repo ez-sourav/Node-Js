27a8b41701951b951a74a89a0629626464090d91
ez-sourav <souravb2003@gmail.com>
2025-12-13 11:06:48 +0530

    commit blog

--- Blog-Application/routes/blog.js
+++ Blog-Application/routes/blog.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const multer = require("multer");
 const path = require("path");
 const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 const router = Router();
 
 const storage = multer.diskStorage({
@@ -22,17 +23,26 @@ router.get("/add-new", (req, res) => {
   });
 });
 
+
+
 router.get("/:id",async (req, res) => {
   const blog = await Blog.findById(req.params.id).populate('createdBy')
-  console.log(blog);
+  const comments = await Comment.find({blogId:req.params.id}).populate('createdBy')
+  console.log(comments);
   return res.render('blog',{
     user:req.user,
     blog,
+    comments,
   })
 });
 
-router.post('/comment/:blogId', (req, res) => {
-
+router.post('/comment/:blogId',async (req, res) => {
+   await Comment.create({
+    content:req.body.content,
+    blogId:req.params.blogId,
+    createdBy:req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`)
 })
 
 router.post("/", upload.single("coverImage"), async (req, res) => {
