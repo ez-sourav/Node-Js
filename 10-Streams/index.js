54145f646119d22c5d3cffaba9b67f37354c9aa8
ez-sourav <souravb2003@gmail.com>
2025-12-15 16:53:46 +0530

    add zlib

--- 10-Streams/index.js
+++ 10-Streams/index.js
@@ -1,15 +1,25 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
+const status = require("express-status-monitor");
+const zlib = require('zlib');
 
 const app = express();
 const PORT = 3000;
 
-app.get('/',(req,res)=>{
-    fs.readFile('./sample.txt',(err,data)=>{
-        res.end(data);
-    });
+app.use(status());
+
+fs.createReadStream('./sample.txt').pipe(zlib.createGzip().pipe(fs.createWriteStream('./sample.zip')))
+
+app.get("/", (req, res) => {
+  const stream = fs.createReadStream("./sample.txt", "utf-8");
+  stream.on("data", (chunk) => {
+    res.write(chunk);
+  });
+  stream.on('end',()=>{
+    res.end();
+  })
 });
 
-app.listen(PORT,()=>{
-    console.log("Sever Started at",PORT);
-})
+app.listen(PORT, () => {
+  console.log("Sever Started at", PORT);
+});
