6094621dba18531ee8d28fb71d033053cc5d4180
ez-sourav <souravb2003@gmail.com>
2025-12-21 18:52:55 +0530

    api use

--- Blog-Application/utils/getLocationFromIP.js
+++ Blog-Application/utils/getLocationFromIP.js
@@ -1,21 +1,9 @@
-const axios = require("axios");
-
-const getLocationFromIP = async (ip) => {
-  try {
-    const res = await axios.get(`https://ipapi.co/${ip}/json/`);
-
-    return {
-      city: res.data.city || "Unknown",
-      region: res.data.region || "Unknown",
-      country: res.data.country_name || "Unknown",
-    };
-  } catch (error) {
-    return {
-      city: "Unknown",
-      region: "Unknown",
-      country: "Unknown",
-    };
-  }
+const getLocationFromIP = async () => {
+  return {
+    city: "Unknown",
+    region: "Unknown",
+    country: "Unknown",
+  };
 };
 
 module.exports = getLocationFromIP;
