77bb7a6d04460210714296abf1e0f3b91cad20d5
ez-sourav <souravb2003@gmail.com>
2025-12-22 21:40:18 +0530

    add logout route

--- Authentication-Application/index.js
+++ Authentication-Application/index.js
@@ -5,6 +5,9 @@ const app = express();
 const connectDB = require('./config/db')
 const signup = require('./routes/signup')
 const login = require('./routes/login')
+const logout = require('./routes/logout')
+const auth = require('./middleware/auth')
+const cookieParser = require('cookie-parser')
 
 connectDB();
 
@@ -12,9 +15,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/',signup);
 app.use('/',login);
+app.use('/',logout);
 
-app.get('/',(req,res)=>{
-    res.send("Server is running...");
+app.use(cookieParser())
+
+app.get('/dashboard',auth ,(req,res)=>{
+    res.send("Welcome to Dashboard");
 })
 
 app.listen(PORT,(req,res)=>{
