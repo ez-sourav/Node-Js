eaf926a2efd82d457437270da02958fa02f2497e
ez-sourav <souravb2003@gmail.com>
2026-01-05 17:09:44 +0530

    use dynamic routes

--- EJS/index.js
+++ EJS/index.js
@@ -10,6 +10,14 @@ app.set('view engine','ejs');
 app.get('/',(req,res)=>{
     res.render('index');
 })
+app.get('/profiles/:username',(req,res)=>{
+    const user = req.params.username; 
+    res.send(`Hello ${user}`);
+})
+app.get('/profiles/:username/:age',(req,res)=>{
+    const user = req.params; 
+    res.send(`Hello ${user.username} your age is ${user.age}`);
+})
 
 app.listen(3000,()=>{
     console.log("The server is running on port 3000");
