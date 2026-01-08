e07dc7ccf3772b2226e96591d470573f90558e2f
ez-sourav <souravb2003@gmail.com>
2026-01-09 02:13:27 +0530

    add read allusers,delete user routes

--- CURD-With-EJS/index.js
+++ CURD-With-EJS/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const userModel = require('./models/user');
 const app = express();
 
 app.set("view engine","ejs")
@@ -10,8 +11,25 @@ app.use(express.static(path.join(__dirname,"public")));
 app.get('/',(req,res)=>{
     res.render('index')
 })
-app.get('/read',(req,res)=>{
-    res.render('read')
+app.get('/read',async(req,res)=>{
+    const allusers =await userModel.find({})
+    res.render('read',{users:allusers})
+})
+
+app.post('/create',async (req,res)=>{
+    const {name,email,imageurl} = req.body
+    const createdUser = await userModel.create({
+        name,
+        email,
+        imageurl,
+    })
+    res.redirect('/read'); 
+})
+
+app.get('/delete/:id',async (req,res)=>{
+    const userId = req.params.id
+    const deletedUser =await userModel.findOneAndDelete({_id:userId})
+    res.redirect('/read')
 })
 
 app.listen(3000,()=>{
