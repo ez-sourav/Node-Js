89334246cfb1824aa8b65fd4f6cb767bd19ced84
ez-sourav <souravb2003@gmail.com>
2026-01-10 14:52:05 +0530

    jwt

--- Authentication/index.js
+++ Authentication/index.js
@@ -1,38 +1,48 @@
-const cookieParser = require('cookie-parser')
-const express = require('express')
-const app = express()
-const bcrypt = require('bcrypt');
-const port = 3000
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const port = 3000;
 
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    // set cookie
-//     res.cookie("name","sourav")
-//   res.send('Hello World!')
+app.get("/", (req, res) => {
+  // set cookie
+  //     res.cookie("name","sourav")
+  //   res.send('Hello World!')
 
-// password hash 
-    // bcrypt.genSalt(10,(err,salt)=>{
-    //     bcrypt.hash('sourav',salt,(err,hash)=>{
-    //         console.log(hash);
-    //     })
-    // })
-    // res.send("Hashed Password ")
+  // password hash
+  // bcrypt.genSalt(10,(err,salt)=>{
+  //     bcrypt.hash('sourav',salt,(err,hash)=>{
+  //         console.log(hash);
+  //     })
+  // })
+  // res.send("Hashed Password ")
 
-    // compaire Hash password
-    bcrypt.compare('sourav ', '$2b$10$AwkGFEheMeyY1t4XvC45ZO9uHSd5rYIsgPtDGarOr5k3j1LFfrm2.', function(err, result) {
-    console.log(result);
+  // compaire Hash password
+  // bcrypt.compare(
+  //   "sourav ",
+  //   "$2b$10$AwkGFEheMeyY1t4XvC45ZO9uHSd5rYIsgPtDGarOr5k3j1LFfrm2.",
+  //   function (err, result) {
+  //     console.log(result);
+  //   }
+  // );
+  // res.send("Compaire");
+  let token = jwt.sign({email:"souravb@gmail.con"},"secret");
+  res.cookie("token",token);
+  console.log(token);
+  res.send("Send Token")
 });
-res.send("Compaire")
-})
-app.get('/read', (req, res) => {
-    // read cookies
-    console.log(req.cookies);
-  res.send('Hello World!')
-})
-
-
+app.get("/read", (req, res) => {
+  // read cookies
+  console.log(req.cookies.token);
+  const data = jwt.verify(req.cookies.token,"secret")
+  res.json({
+    data
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Serever Started At Port ${port}`)
-})
+  console.log(`Serever Started At Port ${port}`);
+});
