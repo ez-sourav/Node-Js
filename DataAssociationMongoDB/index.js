62accd1806891df461c9f79892a88801d3a91cf2
ez-sourav <souravb2003@gmail.com>
2026-01-11 21:04:05 +0530

    add post and user

--- DataAssociationMongoDB/index.js
+++ DataAssociationMongoDB/index.js
@@ -17,6 +17,18 @@ app.get('/create', async(req, res) => {
 
   res.send(user);
 });
+app.get('/post/create', async(req, res) => {
+  const post =await postModel.create({
+    postdata:"This is a Post Title",
+    user:"696392e04a52f68b987e30a4",
+    email:"sourav@gmail.com",
+  })
+  let user =await userModel.findOne({ _id:"696392e04a52f68b987e30a4"})
+  user.posts.push(post._id);
+   await user.save();
+
+  res.send({post,user});
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
