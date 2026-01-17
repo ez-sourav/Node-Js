28e9dcdf876c90c202fdabb2a17daee2d47a0bdc
ez-sourav <souravb2003@gmail.com>
2026-01-17 13:45:52 +0530

    add owner user

--- Projects/BagBuzz/routes/ownersRouter.js
+++ Projects/BagBuzz/routes/ownersRouter.js
@@ -1,8 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.send("Hey it's Working");
-})
+const owerModel = require("../models/owner-model");
+
+router.get("/", (req, res) => {
+  res.send("Hey it's Working");
+});
+
+if (process.env.NODE_ENV == "development") {
+  router.post("/create", async (req, res) => {
+    const owners = await owerModel.find();
+    if (owners.length > 0) {
+      return res
+        .status(503)
+        .send("You don't have permission to create new owner.");
+    }
+    const {fullname,email,password} = req.body;
+   const createdOwner = await owerModel.create({
+      fullname,
+      email,
+      password,
+    });
+    res.status(201).send(createdOwner);
+  });
+}
 
 module.exports = router;
