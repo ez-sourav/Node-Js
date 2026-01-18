f64a36aa3f0bede377a1581cacb5036e50bfa67c
ez-sourav <souravb2003@gmail.com>
2026-01-18 18:00:02 +0530

    add users login register

--- Projects/BagBuzz/routes/usersRouter.js
+++ Projects/BagBuzz/routes/usersRouter.js
@@ -1,8 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const isloggedin = require("../middlewares/isLoggedIn");
+const {
+  registerUser,
+  loginUser,
+  logout,
+} = require("../controllers/authController");
 
-router.get('/',(req,res)=>{
-    res.send("Hey it's Working");
-})
+router.get("/", (req, res) => {
+  res.send("Hey it's Working");
+});
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logout);
 
 module.exports = router;
