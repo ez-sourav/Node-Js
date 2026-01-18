f7ec3002e96e81008fd616d11a4dcba191f04aea
ez-sourav <souravb2003@gmail.com>
2026-01-18 17:58:54 +0530

    add index router

--- Projects/BagBuzz/routes/index.js
+++ Projects/BagBuzz/routes/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const {registerUser,loginUser} = require('../controllers/authController')
+const isloggedin = require('../middlewares/isLoggedIn');
+const productModel = require("../models/product-model");
 
 router.get("/", (req, res) => {
-  res.render("index", {
-    error: "",
-  });
+  const error = req.flash('error');
+  res.render("index", {error});
 });
 
-router.post("/register",registerUser);
+router.get('/shop',isloggedin,async(req,res)=>{
+  const products = await productModel.find();
+  res.render('shop',{products});
+});
 
-router.post("/login",loginUser);
-
+router.get('/logout',isloggedin,(req,res)=>{
+  res.render('shop');
+})
 module.exports = router;
