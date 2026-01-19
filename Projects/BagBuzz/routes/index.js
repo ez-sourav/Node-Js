88349a1a0eb1841bf0734981ec19053d66d83aa7
ez-sourav <souravb2003@gmail.com>
2026-01-19 20:00:50 +0530

    add cart page

--- Projects/BagBuzz/routes/index.js
+++ Projects/BagBuzz/routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const isloggedin = require('../middlewares/isLoggedIn');
 const productModel = require("../models/product-model");
+const userModel = require("../models/user-model");
 
 router.get("/", (req, res) => {
   const error = req.flash('error');
@@ -10,7 +11,22 @@ router.get("/", (req, res) => {
 
 router.get('/shop',isloggedin,async(req,res)=>{
   const products = await productModel.find();
-  res.render('shop',{products});
+  const success = req.flash('success')
+  res.render('shop',{products,success});
+});
+
+router.get('/cart',isloggedin,async(req,res)=>{
+  const user = await userModel.findOne({email:req.user.email}).populate('cart')
+  const bill = (Number(user.cart[0].price)+20)-Number(user.cart[0].discount)
+  res.render('cart',{user,bill});
+});
+
+router.get('/addtocart/:productid',isloggedin,async(req,res)=>{
+  const user = await userModel.findOne({email:req.user.email})
+  user.cart.push(req.params.productid);
+  await user.save();
+  req.flash('success',"Added to Cart")
+  res.redirect('/shop')
 });
 
 router.get('/logout',isloggedin,(req,res)=>{
