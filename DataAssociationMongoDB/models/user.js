e9782e53fc3216c828337c54fa6ce62c459714ea
ez-sourav <souravb2003@gmail.com>
2026-01-21 19:34:51 +0530

    commit

--- DataAssociationMongoDB/models/user.js
+++ DataAssociationMongoDB/models/user.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/mydatabase') 
-.then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-});
+mongoose.connect('mongodb://localhost:27017/mydatabase')
+    .then(() => {
+        console.log('Connected to MongoDB');
+    }).catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
 
 
 const userSchema = mongoose.Schema({
     name: String,
-    email:String,
-    age:Number,
-    posts:[
+    email: String,
+    age: Number,
+    posts: [
         {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:'post'
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'post'
         }
     ]
 });
