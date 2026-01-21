a9e56ad941869822fb403fadee5cad123134c298
ez-sourav <souravb2003@gmail.com>
2026-01-21 19:37:44 +0530

    commit

--- Authentication-Application/config/db.js
+++ Authentication-Application/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log("MongoDB Connected:",conn.connection.host);
+    console.log("MongoDB Connected:", conn.connection.host);
   } catch (err) {
     console.log("Error in MongoDB :", err.message);
   }
