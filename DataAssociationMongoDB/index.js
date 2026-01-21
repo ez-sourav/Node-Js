119d47cf24448bbdb857fa12e9ee9d6393180007
ez-sourav <souravb2003@gmail.com>
2026-01-21 19:35:02 +0530

    commit

--- DataAssociationMongoDB/index.js
+++ DataAssociationMongoDB/index.js
@@ -8,26 +8,26 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/create', async(req, res) => {
-  const user =await userModel.create({
-    name:"Sourav Biswas",
-    age:22,
-    email:"sourav@gmail.com",
+app.get('/create', async (req, res) => {
+  const user = await userModel.create({
+    name: "Sourav Biswas",
+    age: 22,
+    email: "sourav@gmail.com",
   })
 
   res.send(user);
 });
-app.get('/post/create', async(req, res) => {
-  const post =await postModel.create({
-    postdata:"This is a Post Title",
-    user:"696392e04a52f68b987e30a4",
-    email:"sourav@gmail.com",
+app.get('/post/create', async (req, res) => {
+  const post = await postModel.create({
+    postdata: "This is a Post Title",
+    user: "696392e04a52f68b987e30a4",
+    email: "sourav@gmail.com",
   })
-  let user =await userModel.findOne({ _id:"696392e04a52f68b987e30a4"})
+  let user = await userModel.findOne({ _id: "696392e04a52f68b987e30a4" })
   user.posts.push(post._id);
-   await user.save();
+  await user.save();
 
-  res.send({post,user});
+  res.send({ post, user });
 });
 
 app.listen(PORT, () => {
